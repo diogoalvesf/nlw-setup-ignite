6df3adfa8fb76fa53f2a2a4aa2368b486915fbba
Diogo Alves <diogo.alvesf88@gmail.com>
2023-02-05 12:58:42 -0300

    feat: configured cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,17 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-  return "Hello, world!";
+app.register(cors);
+
+app.get("/habits", async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
 });
 
 app.listen({
